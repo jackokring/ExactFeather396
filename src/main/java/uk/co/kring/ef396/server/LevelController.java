5465afc0602a9bb3bcb0dfefaa1e642eeb75291c
jackokring <jackokring@gmail.com>
2022-02-24 16:38:21 +0000

    Debug stuff for mappings

--- src/main/java/uk/co/kring/ef396/server/LevelController.java
+++ src/main/java/uk/co/kring/ef396/server/LevelController.java
@@ -1,6 +1,11 @@
 package uk.co.kring.ef396.server;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,5 +17,37 @@ public class LevelController {
     @SubscribeEvent
     public static void onCraftingTableOpen(ServerChatEvent event) {
         // Your Code Here
+    }
+
+    @SubscribeEvent
+    public static void missingItemMappings(final RegistryEvent.MissingMappings<Item> event) {
+        event.getAllMappings().forEach(missing -> {
+            ExactFeather.LOGGER.warn("\"" + missing.key + "\" is a missing registry item mapping!");
+            missing.ignore();
+        });//debug missing mappings
+    }
+
+    @SubscribeEvent
+    public static void missingBlockMappings(final RegistryEvent.MissingMappings<Block> event) {
+        event.getAllMappings().forEach(missing -> {
+            ExactFeather.LOGGER.warn("\"" + missing.key + "\" is a missing registry block mapping!");
+            missing.ignore();
+        });//debug missing mappings
+    }
+
+    @SubscribeEvent
+    public static void missingEntityMappings(final RegistryEvent.MissingMappings<EntityType<?>> event) {
+        event.getAllMappings().forEach(missing -> {
+            ExactFeather.LOGGER.warn("\"" + missing.key + "\" is a missing registry entity mapping!");
+            missing.ignore();
+        });//debug missing mappings
+    }
+
+    @SubscribeEvent
+    public static void missingSoundMappings(final RegistryEvent.MissingMappings<SoundEvent> event) {
+        event.getAllMappings().forEach(missing -> {
+            ExactFeather.LOGGER.warn("\"" + missing.key + "\" is a missing registry sound mapping!");
+            missing.ignore();
+        });//debug missing mappings
     }
 }
