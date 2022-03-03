940097d6e6652f6360edc1c75aecbfb3173912e4
jackokring <jackokring@gmail.com>
2022-03-03 12:43:02 +0000

    Exception reason recovery

--- src/main/java/uk/co/kring/ef396/utilities/exceptions/BaseCodeException.java
+++ src/main/java/uk/co/kring/ef396/utilities/exceptions/BaseCodeException.java
@@ -1,5 +1,7 @@
 package uk.co.kring.ef396.utilities.exceptions;
 
+import uk.co.kring.ef396.ExactFeather;
+
 public abstract class BaseCodeException extends Exception  {
 
     public final void emote() {
@@ -15,8 +17,19 @@ public abstract class BaseCodeException extends Exception  {
     }
 
     public static final void throwAssist(String why, BaseCodeException e) {
-
         throw new RuntimeException(why, e);
+    }
+
+    public static final BaseCodeException catchAssist(String because, RuntimeException e) {
+        String why = e.getMessage();
+        BaseCodeException f = null;
+        try {
+             f = (BaseCodeException) e.getCause();
+        } catch(ClassCastException g) {
+            ExactFeather.LOGGER.info("Not a BaseCodeException, " + because);
+        }
+        ExactFeather.LOGGER.info(why + ", " + because);//log
+        return f;
     }
 
     protected abstract void actionTry() throws BaseCodeException;
